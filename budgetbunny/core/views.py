3a9c0563ded09fa64158b2d52434c2ccf726a413
glogloria <theglocode@gmail.com>
2026-02-21 19:09:00 -0500

    Fixed Django not loading html page

---
+++ budgetbunny/core/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def show_index_page(request):
+    return render(request, "core/index.html")
